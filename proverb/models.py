4af70579fdf5bbe18e5b24ae1c50db563541ff05
ChrisT-CC <cristaecc@gmail.com>
2022-02-15 16:03:52 +0000

    Create Proverbs model, make migrations, rename it to Proverb and migrate

--- proverb/models.py
+++ proverb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Proverb(models.Model):
+    """Proverbs Django model"""
+    content = models.CharField(max_length=200, unique=True)
+    meaning = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    favorite = models.BooleanField(default=False)
