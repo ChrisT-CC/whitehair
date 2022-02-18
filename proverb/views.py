c65a1187d75c2334798b21c6890259687b9650e3
ChrisT-CC <cristaecc@gmail.com>
2022-02-18 22:15:20 +0000

    Modify home 'view' and 'template' to show a random proverb form database

--- proverb/views.py
+++ proverb/views.py
@@ -1,7 +1,19 @@
 """ proverb app views """
+import random
 from django.shortcuts import render
+from .models import Proverb
 
 
 def home(request):
     """ Home view """
-    return render(request, 'home.html')
+    max_records = Proverb.objects.all().count()
+    random_id = random.randint(1, max_records)
+    random_proverb_obj = Proverb.objects.get(id=random_id)
+    print(f'id = {random_id}')            # testing
+    print(f'random_proverb_obj = {random_proverb_obj}')   # testing
+    context = {
+        'content': random_proverb_obj.content,
+        'meaning': random_proverb_obj.meaning,
+    }
+    print(f'context is: {context}')
+    return render(request, 'home.html', context)
