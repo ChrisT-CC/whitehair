04610d8fce0906ab0e594b20bd2ec6b125f4c896
ChrisT-CC <cristaecc@gmail.com>
2022-02-23 11:56:13 +0000

    Clean code

--- proverb/views.py
+++ proverb/views.py
@@ -12,27 +12,21 @@ def home(request):
     generates a random proverb and meaning
     """
     max_records = Proverb.objects.all()
-    print(f'max_records: {max_records}')                    # testing
     records_ids = [max_record.id for max_record in max_records]
-    print(f'records_ids: {records_ids}')                    # testing
     random_id = random.choice(records_ids)
-    print(f'random_id: {random_id}')                        # testing
     random_proverb_obj = Proverb.objects.get(id=random_id)
-    print(f'random_proverb_obj = {random_proverb_obj}')     # testing
 
     context = {
         'id': random_proverb_obj.id,
         'content': random_proverb_obj.content,
         'meaning': random_proverb_obj.meaning,
     }
-    print(f'context is: {context}')                         # testing
     return render(request, 'home.html', context)
 
 
 def list_proverbs(request):
     """ generates a list with all proverbs """
     all_proverbs = Proverb.objects.all()
-    print(f'all proverbs: {all_proverbs}')                  # testing
     context = {'all_proverbs': all_proverbs}
     return render(request, 'proverbs.html', context)
 
@@ -57,7 +51,6 @@ def add_proverb(request):
 @login_required
 def edit_proverb(request, pk):
     """ edit proverb view """
-    print(f'edit_id={pk}')                                  # testing
     proverb = get_object_or_404(Proverb, id=pk)
     # add POST functionality
     if request.method == 'POST':
